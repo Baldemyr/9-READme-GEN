a536f2d5fd8e28ba19a314f6db19ec6b7935de32
Baldemyr <son_of_loki@hotmail.com>
2022-07-18 21:09:07 -0400

    Basically functional except for licence badges. Only MIT or none work

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ inquirer
       type: "list",
       message: "License:",
       name: "license",
-      choices: ["MIT", "Apache 2.0", "MPL 2.0", "Boost", "GPL v3", "None"],
+      choices: ["MIT", "Apache 2.0", "MPL 2.0", "GPL v3", "None"],
     },
     {
       type: "input",
